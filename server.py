25ccf5e0e5a31721f265775f7e89a14f0e920f00
Pedro Roldan <pedro.roldan@gmail.com>
2020-03-27 19:13:20 +0000

    initial commit

--- server.py
+++ server.py
@@ -10,3 +10,47 @@ BUFFERSIZE = 1024
 ADDR = (HOST, PORT)
 SERVER = socket(AF_INET, SOCK_STREAM)
 SERVER.bind(ADDR)
+
+
+def acceptIncomingConnections():
+    while True:
+        client, client_address = SERVER.accept()
+        print("%s:%s has connected." % client_address)
+        client.send(bytes("Greetings from Viper! Now type your name and press enter!", "utf8"))
+        addresses[client] = client_address
+        Thread(target=handleClient, args=(client,)).start()
+
+
+def handleClient(client):  # Takes client socket as argument.
+    name = client.recv(BUFFERSIZE).decode("utf8")
+    welcome = 'Welcome %s! If you ever want to quit, type {quit} to exit.' % name
+    client.send(bytes(welcome, "utf8"))
+    msg = "%s has joined the chat!" % name
+    broadcast(bytes(msg, "utf8"))
+    clients[client] = name
+
+    while True:
+        msg = client.recv(BUFFERSIZE)
+        if msg != bytes("{quit}", "utf8"):
+            broadcast(msg, name+": ")
+        else:
+            client.send(bytes("{quit}", "utf8"))
+            client.close()
+            del clients[client]
+            broadcast(bytes("%s has left the chat." % name, "utf8"))
+            break
+
+
+def broadcast(msg, prefix=""):  # prefix is for name identification.
+    for sock in clients:
+        sock.send(bytes(prefix, "utf8")+msg)
+
+
+if __name__ == "__main__":
+    SERVER.listen(5)
+    print("Waiting for connection...")
+    ACCEPT_THREAD = Thread(target=acceptIncomingConnections())
+    ACCEPT_THREAD.start()
+    ACCEPT_THREAD.join()
+    SERVER.close()
+
